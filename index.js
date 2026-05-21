717fbae78855988996f115d57b4ab4b097de53f0
Fathan Mohamad <fathanmohamad17@gmail.com>
2026-05-21 19:43:29 +0800

    adjustment part 1

--- index.js
+++ index.js
@@ -348,6 +348,7 @@ function startBattleTimer(roomId) {
     }, 1000);
 }
 
-server.listen(3000, () => {
-  console.log("Server jalan di port 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server jalan di port ${PORT}`);
 });
